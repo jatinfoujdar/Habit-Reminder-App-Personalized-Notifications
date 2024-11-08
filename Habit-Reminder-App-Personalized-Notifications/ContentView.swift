df52abfad1abc6889cbb4bc706f592ae35be6827
Jatin Foujdar <jatin321001@gmail.com>
2024-11-08 23:38:16 +0530

    Initial Commit

---
+++ Habit-Reminder-App-Personalized-Notifications/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Habit-Reminder-App-Personalized-Notifications
+//
+//  Created by jatin foujdar on 08/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Habit-Reminder-App-Personalized-Notifications/Habit_Reminder_App_Personalized_NotificationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Habit_Reminder_App_Personalized_NotificationsApp.swift
+//  Habit-Reminder-App-Personalized-Notifications
+//
+//  Created by jatin foujdar on 08/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Habit_Reminder_App_Personalized_NotificationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
