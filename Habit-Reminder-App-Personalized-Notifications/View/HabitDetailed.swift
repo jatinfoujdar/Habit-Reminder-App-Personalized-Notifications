26a8f2b54f1433797bfe2ad3c269d70c06f35292
Jatin Foujdar <jatin321001@gmail.com>
2024-11-10 00:02:00 +0530

    habit detailed view commit

--- Habit-Reminder-App-Personalized-Notifications/View/HabitDetailed.swift
+++ Habit-Reminder-App-Personalized-Notifications/View/HabitDetailed.swift
@@ -8,11 +8,31 @@
 import SwiftUI
 
 struct HabitDetailed: View {
+    var habit: HabitModel
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+      
+               VStack {
+                   Text(habit.name)
+                       .font(.largeTitle)
+                       .fontWeight(.bold)
+                   
+                   Text("Reminder: \(habit.reminderTime, style: .time)")
+                       .font(.title)
+                       .foregroundColor(.gray)
+                   
+                   Text("Completion Streak: \(habit.completionStreak)")
+                       .font(.title2)
+                   
+                   Text("Missed Days: \(habit.missedDays)")
+                       .font(.title2)
+                   
+                   Spacer()
+               }
+               .padding()
+               .navigationTitle("Habit Details")
+           }
 }
 
 #Preview {
-    HabitDetailed()
+    HabitDetailed(habit: HabitModel(name: "Drink Water", reminderTime: Date(), completionStreak: 5, missedDays: 2))
 }
