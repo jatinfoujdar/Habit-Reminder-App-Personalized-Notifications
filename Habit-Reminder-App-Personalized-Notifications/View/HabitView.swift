c14e6c9b0f27c4c593d0d22fef5d17cea3e6e883
Jatin Foujdar <jatin321001@gmail.com>
2024-11-10 12:25:30 +0530

    link detailed link commit

--- Habit-Reminder-App-Personalized-Notifications/View/HabitView.swift
+++ Habit-Reminder-App-Personalized-Notifications/View/HabitView.swift
@@ -4,61 +4,64 @@ struct HabitView: View {
     @State private var habit = ""
     @State private var reminderDate = Date()
     @State private var savedHabits: [HabitModel] = []
-
+    
     var body: some View {
-        VStack(spacing: 20) {
-            Text("Habit Reminder")
-                .fontWeight(.bold)
-                .font(.largeTitle)
-            
-            TextField("Enter habit", text: $habit)
-                .padding()
-                .background(Color(.systemGray6))
-                .cornerRadius(8)
-                .padding(.horizontal)
-            
-            DatePicker("Set Reminder Date", selection: $reminderDate, displayedComponents: [.date, .hourAndMinute])
-                .datePickerStyle(DefaultDatePickerStyle())
-                .padding(.horizontal)
-            
-            Button(action: {
-                if !habit.isEmpty {
-                    let newHabit = HabitModel(name: habit, reminderTime: reminderDate, completionStreak: 0, missedDays: 0)
-                    savedHabits.append(newHabit)
-                    habit = ""
-                }
-            }) {
-                Text("Save Habit")
-                    .fontWeight(.semibold)
+        NavigationView {
+            VStack(spacing: 20) {
+                                
+                TextField("Enter habit", text: $habit)
                     .padding()
-                    .frame(maxWidth: .infinity)
-                    .background(Color.blue)
-                    .foregroundColor(.white)
-                    .cornerRadius(10)
+                    .background(Color(.systemGray6))
+                    .cornerRadius(8)
                     .padding(.horizontal)
-            }
-            
-            
-            if !savedHabits.isEmpty {
-                VStack(alignment: .leading, spacing: 10) {
-                    Text("Saved Habits:")
-                        .font(.headline)
-                    
-                    
-                    ForEach(savedHabits, id: \.id) { habit in
-                        VStack(alignment: .leading) {
-                            Text("- \(habit.name)")
-                                .font(.subheadline)
-                            Text("Reminder: \(habit.reminderTime, style: .time)")
-                                .font(.caption)
-                                .foregroundColor(.gray)
-                        }
+                
+                DatePicker("Set Reminder Date", selection: $reminderDate, displayedComponents: [.date, .hourAndMinute])
+                    .datePickerStyle(DefaultDatePickerStyle())
+                    .padding(.horizontal)
+                
+                Button(action: {
+                    if !habit.isEmpty {
+                        let newHabit = HabitModel(name: habit, reminderTime: reminderDate, completionStreak: 0, missedDays: 0)
+                        savedHabits.append(newHabit)
+                        habit = ""
                     }
-                    .padding(.top, 20)
+                }) {
+                    Text("Save Habit")
+                        .fontWeight(.semibold)
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                        .padding(.horizontal)
                 }
+                
+                if !savedHabits.isEmpty {
+                    VStack(alignment: .leading, spacing: 10) {
+                        Text("Saved Habits:")
+                            .font(.headline)
+                        
+                        ForEach(savedHabits) { habit in
+                            NavigationLink(destination: HabitDetailed(habit: habit)) {
+                                VStack(alignment: .leading) {
+                                    Text("- \(habit.name)")
+                                        .font(.subheadline)
+                                    Text("Reminder: \(habit.reminderTime, style: .time)")
+                                        .font(.caption)
+                                        .foregroundColor(.gray)
+                                }
+                                .padding(.vertical, 5)
+                            }
+                            .buttonStyle(PlainButtonStyle())
+                        }
+                        .padding(.top, 20)
+                    }
+                }
+                
+                Spacer()
             }
+            .navigationTitle("Habit Tracker")
         }
-        Spacer()
     }
 }
 
